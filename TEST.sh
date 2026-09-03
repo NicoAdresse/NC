e66033aacec9a100e7d59e9eca2df46b50c37152
NicoAdresse <nicobsc4@yahoo.com>
2026-09-03 06:40:09 +0200

    Updated GCC compilation for leaner binaries

--- TEST.sh
+++ TEST.sh
@@ -24,7 +24,7 @@ EXECUTABLE_NAME="${FILENAME%.c}"
 echo "Starting script for test: $TEST_PATH"
 
 echo "Compiling $TEST_PATH..."
-gcc -Wall -Wextra -std=c11 -Isrc "$TEST_PATH" -o "build/$EXECUTABLE_NAME"
+gcc -Wall -Wextra -std=c11 -Os -ffunction-sections -fdata-sections -fno-asynchronous-unwind-tables -fno-unwind-tables -Isrc "$TEST_PATH" -Wl,--gc-sections -s -o "build/$EXECUTABLE_NAME"
 echo "Done!"
 
 echo "Executing $EXECUTABLE_NAME"
