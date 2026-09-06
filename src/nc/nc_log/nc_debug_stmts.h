677d9b364f8c34d323961a63109dd334b9f6aa79
NicoAdresse <nicobsc4@yahoo.com>
2026-09-06 12:42:05 +0200

    Made nc_assert and nc_debug_log more portable.

--- src/nc/nc_log/nc_debug_stmts.h
+++ src/nc/nc_log/nc_debug_stmts.h
@@ -13,12 +13,23 @@
 
 #include "nc_print_stmts.h"
 
+#if defined(__GNUC__) || defined(__clang__)
+    #define NC_UNLIKELY(x) __builtin_expect(!!(x), 0)
+#else
+    #define NC_UNLIKELY(x) (x)
+#endif
+
 #if !defined(NDEBUG) && !defined(NC_RELEASE)
-    #define nc_debug_log(fmt, ...) nc_println_debug("[DEBUG] " fmt, ##__VA_ARGS__)
+    #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L
+        #define nc_debug_log(fmt, ...) nc_println_debug("[DEBUG] " fmt __VA_OPT__(,) __VA_ARGS__)
+    #else
+        #define nc_debug_log(fmt, ...) nc_println_debug("[DEBUG] " fmt, ##__VA_ARGS__)
+    #endif
+
     #define nc_assert(condition, msg) \
         do { \
-            if (!(condition)) { \
-                nc_println_err("Assertion failed: %s (File: %s, Line: %d)", msg, __FILE__, __LINE__); \
+            if (NC_UNLIKELY(!(condition))) { \
+                nc_println_err("Assertion failed: %s (File: %s, Line: %ld)", msg, __FILE__, (long)__LINE__); \
                 abort(); \
             } \
         } while (0)
