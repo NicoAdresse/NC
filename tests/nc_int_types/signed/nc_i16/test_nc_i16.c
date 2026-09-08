35ce2fc286cec24e56883bfd8a939f7336c30677
NicoAdresse <nicobsc4@yahoo.com>
2026-09-08 05:41:00 +0200

    TESTING NOT FINISHED: WIP

--- tests/nc_int_types/signed/nc_i16/test_nc_i16.c
+++ tests/nc_int_types/signed/nc_i16/test_nc_i16.c
@@ -21,4 +21,7 @@ int main(void) {
     printf("NC_GET_VAL(NC_CHECKED_SUB) [x, y]: %d\n", nc_get_val_int(nc_checked_sub_int(x, y)));
     printf("NC_GET_VAL(NC_CHECKED_MUL) [x, y]: %d\n", nc_get_val_int(nc_checked_mul_int(x, y)));
     printf("NC_GET_VAL(NC_CHECKED_DIV) [x, y]: %d\n", nc_get_val_int(nc_checked_div_int(x, y, 0)));
+
+    nc_assert(nc_get_val_int(nc_checked_mod_i16(x, y, 0)) == nc_get_val_int(nc_int_convert_to_i16(-150)), "Should be negative 150.");
+    nc_debug_log("If you see this, this means the assertion was successful.");
 }
--- tests/nc_int_types/signed/nc_i8/test_nc_i8.c
+++ tests/nc_int_types/signed/nc_i8/test_nc_i8.c
@@ -23,4 +23,7 @@ int main(void) {
     printf("NC_GET_VAL(NC_CHECKED_MUL) [x, y]: %d\n", nc_get_val_int(nc_checked_mul_int(x, y)));
     printf("NC_GET_VAL(NC_CHECKED_DIV zero error check) [x, zero]: %d\n", nc_get_val_int(nc_checked_div_int(x, zero, 0)));
     printf("NC_GET_VAL(NC_CHECKED_DIV no zero error check) [x, zero]: %d\n", nc_get_val_int(nc_checked_div_int(x, zero, 1)));
+    
+    nc_assert(nc_get_val_int(nc_checked_mod_i8(x, y, 0)) == nc_get_val_int(nc_int_convert_to_i8(-5)), "Should be negative five.");
+    nc_debug_log("If you see this, this means the assertion was successful.");
 }
