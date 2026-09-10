6349cc10a436b7a365948b0b0af77ebb8521d60e
NicoAdresse <nicobsc4@yahoo.com>
2026-09-10 17:03:17 +0200

    Added missing modulo method to nc_ptr_u16

--- src/nc/nc_int/nc_ptr_u16/nc_ptr_u16.h
+++ src/nc/nc_int/nc_ptr_u16/nc_ptr_u16.h
@@ -88,6 +88,26 @@ static inline nc_ptr_u16 nc_checked_div_ptr_u16(nc_ptr_u16 ptr_num, nc_u16 val, 
     return nc_new_ptr_u16((nc_u16){.val = quot});
 }
 
+/* Checked Modulo -> nc_ptr_u16 */
+static inline nc_ptr_u16 nc_checked_mod_ptr_u16(nc_ptr_u16 ptr_num, nc_u16 val, int is_zero_denominator_allowed) {
+    if (!ptr_num.ptr) {
+        send_null_pointer_error_msg(IS_UNSIGNED, "nc_ptr_u16");
+        return (nc_ptr_u16){.ptr = NULL};
+    }
+
+    nc_u16 number_one = *ptr_num.ptr;
+
+    if (val.val == 0) {
+        if (!is_zero_denominator_allowed) {
+            send_zero_denominator_error_msg(IS_UNSIGNED, (int64_t)number_one.val, (int64_t)val.val);
+        }
+        return nc_new_ptr_u16((nc_u16){.val = 0});
+    }
+
+    uint16_t rem = number_one.val % val.val;
+    return nc_new_ptr_u16((nc_u16){.val = rem});
+}
+
 /* nc_get_size */
 static inline size_t nc_get_size_ptr_u16() { return sizeof(nc_ptr_u16); }
 
