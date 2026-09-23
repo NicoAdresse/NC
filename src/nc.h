61d0615f2353549e8848c6a35e0f554102a862a8
NicoAdresse <nicobsc4@yahoo.com>
2026-09-23 19:10:53 +0200

    Added a nc.h

---
+++ src/nc.h
@@ -0,0 +1,16 @@
+/* src/nc.h */
+
+/*
+    SPDX-License-Identifier: MIT
+    Copyright (c) 2026 Nico Erdmann
+    The ultimate duct type for all primitives, methods and whatnot.
+*/
+
+#ifndef NC_H
+#define NC_H
+
+#include "nc/nc_float_types.h"
+#include "nc/nc_int_types.h"
+#include "nc/nc_log.h"
+
+#endif /* NC_H */
